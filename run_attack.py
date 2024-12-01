6f24fe3c3ba406fb591a05de49558f003d6ebb5a
Munia <mushfikaikfat1998@gmail.com>
2024-12-01 23:51:06 +0100

    adding errors

--- run_attack.py
+++ run_attack.py
@@ -99,9 +99,9 @@ if __name__ == '__main__':
         #Uncomment the following For medium size property
         #default ='[0.00,0.001,0.002,0.003,0.004,0.005,0.006,0.007,0.008,0.009,0.01]'
         #Uncomment the following For large size property
-        #default ='[0.0,0.005, 0.01,0.015, 0.02, 0.025, 0.03,0.035, 0.04, 0.045, 0.05]'
+        default ='[0.0, 0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1]'
         #Uncomment the following For small size property
-        default= '[0.00,0.0001,0.0002,0.0003,0.0004,0.0005,0.0006,0.0007,0.0008]'
+        #default= '[0.00,0.0001,0.0002,0.0003,0.0004,0.0005,0.0006,0.0007,0.0008]'
         #default='[0.000,0.001 ]'
     )
     
@@ -247,7 +247,7 @@ if __name__ == '__main__':
         poisoning_rates.append(key*100)
         accuracies.append(avg_success[key])
     
-     # **Modified Section for Dynamic Title**
+    # **Modified Section for Dynamic Plot**
     # Extract target properties
     target_properties = arguments["targetproperties"]
     # Multiply t0 and t1 fractions by 100
